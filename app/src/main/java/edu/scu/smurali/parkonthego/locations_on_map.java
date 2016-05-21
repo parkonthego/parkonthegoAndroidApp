b1763874dbb89cec2e9d9fbc1129468e0380f5a8
VARUN RAPARLA <varun.raparla@gmail.com>
2016-05-21 12:25:34 -0700

    added maps nutton in login activity

--- app/src/main/java/edu/scu/smurali/parkonthego/locations_on_map.java
+++ app/src/main/java/edu/scu/smurali/parkonthego/locations_on_map.java
@@ -10,7 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class locations_on_map extends FragmentActivity implements OnMapReadyCallback {
+public class Locations_on_map extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
