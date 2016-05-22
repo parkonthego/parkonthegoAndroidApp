3f9618b9fad96844fc6e3cb7134eaa25f454617d
shruthimurali14 <shruthimurali14@gmail.com>
2016-05-21 17:54:30 -0700

    UI changes field names confirmation and settings page

--- app/src/main/java/edu/scu/smurali/parkonthego/Register.java
+++ app/src/main/java/edu/scu/smurali/parkonthego/Register.java
@@ -1,11 +1,16 @@
 package edu.scu.smurali.parkonthego;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +23,10 @@ public class Register extends AppCompatActivity {
             //  actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setDisplayShowHomeEnabled(true);
             // actionBar.setHomeButtonEnabled(true);
-        }
-        catch(NullPointerException ex){
-            Log.d("Register", "onCreate: Null pointer in action bar "+ex.getMessage());
+        } catch (NullPointerException ex) {
+            Log.d("Register", "onCreate: Null pointer in action bar " + ex.getMessage());
         }
     }
+
+
 }
