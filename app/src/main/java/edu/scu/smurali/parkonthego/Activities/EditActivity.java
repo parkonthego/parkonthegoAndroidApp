967ddd1d32c93d29bf953ef4cb2b18eaa676aa70
shruthimurali14 <shruthimurali14@gmail.com>
2016-06-04 00:31:47 -0700

    confirmation page, strinf values, potrait fixed orientation, reservations and setting

---
+++ app/src/main/java/edu/scu/smurali/parkonthego/Activities/EditActivity.java
@@ -0,0 +1,15 @@
+package edu.scu.smurali.parkonthego.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.scu.smurali.parkonthego.R;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
